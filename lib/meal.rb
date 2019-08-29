10fa2628ca4beb93fb3c6d48db86f4925c443611
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-29 20:18:43 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,8 +1,8 @@
 class Meal
-  attr_accessor :name, :year
+  attr_accessor :waiter, :customer, :total, :tip
   @@all = []
 
-  def initialize(waiter, customer, total, tip)
+  def initialize(waiter, customer, total, tip = 0)
     @waiter = waiter
     @customer = customer
     @total = total
