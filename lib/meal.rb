d6707ae121d1f0b195e99eac74925fa04eaeb3f9
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-29 20:13:04 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,7 +2,10 @@ class Meal
   @@all = []
 
   def initialize(waiter, customer, total, tip)
-
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
   end
 
   def self.all
